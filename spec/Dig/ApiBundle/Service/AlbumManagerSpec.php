e9afd0ba84d073a5093f69d26f8e8297df525c56
Evgeniy Kuzmin <jekccs@gmail.com>
2016-09-20 19:40:08 +0300

    initial spec for album manager

---
+++ spec/Dig/ApiBundle/Service/AlbumManagerSpec.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace spec\Dig\ApiBundle\Service;
+
+use Dig\ApiBundle\Repository\ImageRepository;
+use Doctrine\Common\Persistence\ObjectManager;
+use Knp\Component\Pager\Paginator;
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+class AlbumManagerSpec extends ObjectBehavior
+{
+    function it_is_initializable()
+    {
+        $this->shouldHaveType('DigApiBundle\Service\AlbumManager');
+    }
+
+    function let(Paginator $paginator, ObjectManager $entityManager, ImageRepository $repo)
+    {
+        //$repo->findImagesByAlbumId(Argument::any())->willReturn(new Download());
+        $this->beConstructedWith($paginator, $entityManager);
+    }
+
+    function it_return_album_with_its_images()
+    {
+        $this->getAlbumWithImages(Argument::any(), Argument::any())->shouldHaveKey('album');
+    }
+}
