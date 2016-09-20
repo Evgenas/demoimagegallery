01fbc013fce0953148f68bcc65fee80fa707c3c2
Evgeniy Kuzmin <jekccs@gmail.com>
2016-09-20 13:10:36 +0300

    ApiBundle init

---
+++ src/Dig/ApiBundle/Controller/DefaultController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Dig\ApiBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function indexAction()
+    {
+        return $this->render('DigApiBundle:Default:index.html.twig');
+    }
+}
---
+++ src/Dig/ApiBundle/DigApiBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Dig\ApiBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class DigApiBundle extends Bundle
+{
+}
